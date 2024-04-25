eb4687978422bae8f6cc49c5d00fb09bb2a4ea49
Arseny Bulaev <seni190999@mail.ru>
2024-04-25 20:33:49 +0300

    pointers to members

--- source/pointers to members/main.cpp
+++ source/pointers to members/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+struct S {
+    int x;
+    double y;
+
+    void f(int z){
+        std::cout << x + z;
+    }
+};
+
 int main(){
-    std::cout << "Hello world" << std::endl;
+    // Pointer to field.
+    // Number, storing shift, relative to structure begin.
+    int S::* p = &S::x; 
+    S s{1, 3.14};
+    // .* - operator
+    std::cout << s.*p << std::endl;
+    S * ps = &s;
+    // ->* - operator
+    std::cout << ps->*p << std::endl;
+    // Pointer to method
+    void (S::* pf)(int) = &S::f;
+    (s.*pf)(3);
+    (ps->*pf)(5);
+
     return 0;
 }
